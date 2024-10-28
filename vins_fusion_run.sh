5f6e2fc58f0af8ded4cd996d59fa614bf055104f
Derkai <2363104914@qq.com>
2024-10-28 17:34:23 +0800

    [fix]:fix shell bug.

--- vins_fusion_run.sh
+++ vins_fusion_run.sh
@@ -6,5 +6,13 @@ echo "[START] VINS "
 
 # 通过本脚本文件路径来获取 x152b 项目文件根目录
 PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
+
+VINS_OUTPUT_DIR="${PROJECT_DIR}/src/autonomous_drone_sdk/global_interface/config/vins_fusion/output"
+if [ ! -d "${VINS_OUTPUT_DIR}" ]; then
+    mkdir ${VINS_OUTPUT_DIR}
+else
+    echo "Vins-Fusion output dir is already exit."
+fi
+
 gnome-terminal -- bash -c "source ${PROJECT_DIR}/devel/setup.bash;roslaunch vins swarm_d430_no_rviz.launch"  && sleep 1;
 
--- vins_fusion_setup.sh
+++ vins_fusion_setup.sh
@@ -3,73 +3,19 @@
 # set -e
 
 # 获取项目根路径
-PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
-
-# 创建第三方库存放目录 3rdparty
-if [ ! -d PROJECT_DIR/"3rdparty" ]; then
-    mkdir -p ${PROJECT_DIR}"/3rdparty"
-fi
-TRDPARTY_DIR="${PROJECT_DIR}/3rdparty"
-
-sudo apt-get -y install python3-catkin-tools
-
-# mavros
-sudo apt -y install ros-noetic-mavros
-
-# PCL >= 1.6
-echo "Installing PCL library..."
-sudo apt -y install libpcl-dev
-
-# eigen>= 3.3.4
-echo "Check eigen library..."
-if [ ! -d "${TRDPARTY_DIR}/eigen-3.4.0" ]; then
-    echo "Downloading eigen..."
-    wget -O eigen3.zip https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.zip
-    unzip -q eigen3.zip -d "${TRDPARTY_DIR}"
-    pushd "${TRDPARTY_DIR}/eigen-3.4.0"
-    mkdir build
-    cd build
-    cmake -DBUILD_SHARED_LIBS=TRUE ..
-    make
-    sudo make install
-    sudo ln -s /usr/include/eigen3/Eigen /usr/include/Eigen
-    popd
-    rm eigen3.zip
-else
-    echo "eigen is already installed."
-fi
-
-# ceres<= 2.1.0
-echo "Check ceres library..."
-if [ ! -d "${TRDPARTY_DIR}/ceres-solver-2.1.0" ]; then
-    echo "Downloading ceres..."
-    sudo apt-get -y install liblapack-dev libsuitesparse-dev libcxsparse3 libgflags-dev libgoogle-glog-dev libgtest-dev
-    wget -O ceres-solver.zip https://github.com/ceres-solver/ceres-solver/archive/refs/tags/2.1.0.zip
-    unzip -q ceres-solver.zip -d "${TRDPARTY_DIR}"
-    pushd "${TRDPARTY_DIR}/ceres-solver-2.1.0"
-    mkdir build
-    cd build
-    cmake -DBUILD_SHARED_LIBS=TRUE ..
-    make
-    sudo make install
-    popd
-    rm ceres-solver.zip
-else
-    echo "ceres is already installed."
-fi
-
+PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." && pwd )"
 # vins_fusion
-VINS_FUSION_DIR="${PROJECT_DIR}/src/tasks/vins_fusion"
+VINS_FUSION_DIR="${PROJECT_DIR}/src/vins_fusion"
+
 if [ ! -d "${VINS_FUSION_DIR}" ]; then
     echo "Downloading Vins_Fusion..."
-    git clone https://github.com/hku-mars/IPC.git "${VINS_FUSION_DIR}"
-    cd ${VINS_FUSION_DIR}
-    pushd "${PROJECT_DIR}"
-    catkin_make --source ${PROJECT_DIR}/src/tasks/vins_fusion
-    popd
+    git clone https://gitee.com/Derkai52/vins_fusion.git "${VINS_FUSION_DIR}"
+    # git clone https://github.com/emNavi/vins_fusion.git "${VINS_FUSION_DIR}"
 else
-    echo "IPC is already installed."
+    echo "Vins-Fusion is already installed."
 fi
+
+catkin_make
 
 popd >/dev/null
 
